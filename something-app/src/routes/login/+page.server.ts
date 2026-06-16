ca49226d66262807c51634164159a24b09482e3b
simon-vegsund <sv@norsktest.no>
2026-06-16 09:35:02 +0200

    update to check deployment

--- something-app/src/routes/login/+page.server.ts
+++ something-app/src/routes/login/+page.server.ts
@@ -1,9 +1,10 @@
 import type { Actions } from './$types';
 
-export const actions = {
+/* export const actions = {
 	login: async ({ cookies, request }) => {
 		const data = await request.formData();
 		const email = data.get('email');
 		const password = data.get('password');
 	}
 } satisfies Actions;
+ */
